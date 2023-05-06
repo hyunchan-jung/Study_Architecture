0ba674aa7533da0d9da10bdec3cf0931f43e28e3
hyunchan-jung <tmzkdlqmfn1@naver.com>
2023-05-06 15:06:21 +0900

    refactor: split foo,bar service

--- Service Discovery/ZooKeeper/foo_service/components/main.py
+++ Service Discovery/ZooKeeper/foo_service/components/main.py
@@ -4,12 +4,18 @@ import random
 import requests
 from kazoo.client import KazooClient
 
+HOST = os.getenv('HOST')
+HOSTNAME = os.getenv('HOSTNAME')
+ROLE = os.getenv('ROLE')  # 'caller' or 'callee'
+
+SERVICE_NAME = 'foo-service' if HOSTNAME == 'node-02' else 'bar-service'
+
 zk = KazooClient(hosts='192.168.0.12:2181')
 zk.start()
 
 
-def call():
-    path = '/foo-service'
+def call(service_name: str) -> None:
+    path = '/' + service_name
     services = zk.get_children(path)
 
     selected_service = random.choice(services)
@@ -22,21 +28,22 @@ def call():
 
 
 def some_business_logic():
-    if os.environ.get('ROLE') == 'caller':
-        return call()
+    if ROLE == 'caller':  # node-01
+        return str(call('foo-service')) + str(call('bar-service'))
     else:
-        host = os.environ.get('HOSTNAME')
         return {
-            'host': host
+            'host': HOST,
+            'host_name': HOSTNAME,
+            'service_name': SERVICE_NAME,
         }
 
 
 def startup():
-    if os.environ.get('ROLE') == 'callee':
-        path = '/foo-service'
+    if ROLE == 'callee':  # node-02 and node-03
+        path = '/' + SERVICE_NAME
         zk.ensure_path(path)
         value = str({
-            'host': os.environ.get('HOST'),
+            'host': HOST,
             'port': 8000
         }).encode('utf-8')
         zk.create(path=path + '/server', value=value, ephemeral=True, sequence=True)
